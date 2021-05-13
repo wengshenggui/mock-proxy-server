8e77ca59f13b9987e680ca04bc571fdab6dbb3a4
wengshenggui <wengshenggui@hotmail.com>
2021-05-13 22:58:57 +0800

    use koa-proxies

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Koa = require('koa')
 const app = new Koa()
+const cors = require('koa2-cors');
 const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
@@ -12,19 +13,19 @@ debug('config:', config)
 const proxy = require('@/proxy')
 const router = require('@/routes')
 
-
 // error handler
 onerror(app)
 
 // middleware
+app.use(cors())
 app.use(bodyParser({
   enableTypes:['json', 'form', 'text']
 }))
 app.use(json())
 app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+app.use(require('koa-static')('public'))
 
-app.use(views(__dirname + '/views', {
+app.use(views('views', {
   extension: 'pug'
 }))
 
@@ -32,7 +33,7 @@ app.use(views(__dirname + '/views', {
 app.use(router.routes())
 
 // proxy
-app.use(proxy(config.proxy))
+proxy(app, config.proxy)
 
 // error-handling
 app.on('error', (err, ctx) => {
--- proxy/index.js
+++ proxy/index.js
@@ -1,33 +1,16 @@
 const debug = require('@/utils').debug('proxy');
-const proxy = require('./proxy')()
+const proxy = require('koa-proxies')
 
-async function execProxy (ctx, target, otherOption = {}) {
-  try {
-    ctx.req.body = ctx.request.body
-    await proxy.web(ctx.req, ctx.res, { target: target, ...otherOption })
-  } catch (e) {
-    throw e
-  }
-  return ctx.res.proxyBody || null
-}
-
-module.exports = (target, deal) => {
-  if (!target) {
-    return (ctx, next) => next()
-  }
-  // 存在则做代理
+module.exports = (app, target) => {
+  if (!target) return
+  // 代理
   debug(`=> ${target}`)
   console.log(`代理目标 => ${target}`)
-  return (ctx, next) => next().then(async () => {
-    if (ctx.status !== 404) {
-      return
+  app.use(proxy('*', {
+    target,
+    changeOrigin: true,
+    logs: (ctx, target) => {
+      console.log('proxy: %s %s proxy => %s%s', ctx.req.method, ctx.req.oldPath, target, ctx.req.url)
     }
-    // 仅404做代理
-    try {
-      await execProxy(ctx, target)
-      console.log(`代理成功：${ctx.path} => ${target + ctx.path}`);
-    } catch (e) {
-      console.error('代理失败：', e);
-    }
-  })
+  }))
 }
